d4d247ee71fddf50401d02248247f210eb8cb0db
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2020-10-20 10:55:01 -0300

    creating the CreateOrphanages pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,13 +6,38 @@ const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
+import SelectMapPosition from './pages/createOrphanage/SelectMapPosition';
+import OrphanageData from './pages/createOrphanage/OrphanageData';
+import Header from './components/header';
 
 export default function Routes(){
     return(
         <NavigationContainer>
-            <Navigator screenOptions={{headerShown: false}}>
-                <Screen name="OrphanagesMap" component={OrphanagesMap} />
-                <Screen name="OrphanageDetails" component={OrphanageDetails} />
+            <Navigator screenOptions={{headerShown: false, cardStyle:{ backgroundColor: '#f2f3f5'}}} >
+                <Screen 
+                    name="OrphanagesMap" 
+                    component={OrphanagesMap}                 
+                />
+                
+                <Screen 
+                    name="OrphanageDetails" 
+                    component={OrphanageDetails} 
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Orfanato" />
+                        
+                    }} 
+                />
+                
+                <Screen 
+                    name="SelectMapPosition" 
+                    component={SelectMapPosition} 
+                />
+                
+                <Screen 
+                    name="OrphanageData" 
+                    component={OrphanageData} 
+                />
 
             </Navigator>
         </NavigationContainer>
