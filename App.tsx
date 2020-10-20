0798c30d7a60438964a98b9142988986095c2771
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2020-10-19 21:19:11 -0300

    Upadate map attributes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-      <StatusBar style="auto" />
+    <View style={styles.container}>      
+      <MapView 
+        style={styles.map}  
+        initialRegion={{
+          latitude: -22.8839505,
+          longitude: -43.4123537,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }} 
+      />
     </View>
   );
 }
@@ -15,4 +23,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,    
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
