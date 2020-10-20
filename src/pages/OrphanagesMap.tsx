aea6aba5f2feb70830efca87ca39d075487f4b7f
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2020-10-20 12:47:42 -0300

    Adding search api orphanages

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -25,8 +25,8 @@ export default function OrphanagesMap(){
       })
     },[]);
 
-    function handleNavigateToOrphanageDetails(){
-        navigation.navigate('OrphanageDetails');
+    function handleNavigateToOrphanageDetails(id: number){
+        navigation.navigate('OrphanageDetails', {id} );
     }
     function handleNavigateToCreateOrphanage(){
         navigation.navigate('SelectMapPosition');
@@ -58,9 +58,9 @@ export default function OrphanagesMap(){
                 longitude: orphanage.longitude,
               }}
             >
-              <Callout tooltip={true} onPress={handleNavigateToOrphanageDetails}>
+              <Callout tooltip={true} onPress={() => handleNavigateToOrphanageDetails(orphanage.id)}>
                 <View style={styles.calloutContainer}>
-                  <Text style={styles.calloutText}>Lar das meninas</Text>
+                  <Text style={styles.calloutText}>{orphanage.name}</Text>
                   
                 </View>
               </Callout>
@@ -69,7 +69,7 @@ export default function OrphanagesMap(){
         })}
       </MapView>
       <View style={styles.footer}>
-        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+        <Text style={styles.footerText}>{orphanages.length} orfanatos encontrados</Text>
         <RectButton style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
           <Feather name="plus" size={20} color="#FFF" />
         </RectButton>
