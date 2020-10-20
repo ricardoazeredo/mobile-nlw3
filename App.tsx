110dbfb7d8b517ce39f022952ffddf8a8f390162
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2020-10-19 21:38:11 -0300

    add static Marker in map

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>      
       <MapView 
+        provider={PROVIDER_GOOGLE}
         style={styles.map}  
         initialRegion={{
           latitude: -22.8839505,
@@ -14,7 +17,19 @@ export default function App() {
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
         }} 
-      />
+      >
+        <Marker  
+          icon={mapMarker}
+          coordinate={{
+            latitude: -22.8839505,
+            longitude: -43.4123537,
+          }}
+        >
+          <Callout>
+            <Text>Lar das meninas</Text>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
